3e1091a7c7c2b09ea430fb9c96aa60301839a38d
JulianChu <yulang.chu@gmail.com>
2018-04-11 23:02:31 +0200

    install nodemon and body-parser

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,12 @@ import * as express from "express";
 import users from "./models/user";
 import "./sequelize-ts";
 import { Users2 } from "./models/Users2";
+import * as bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.urlencoded({"extended": true}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.get("/user", async (req: express.Request, res: express.Response) => {
   const user: any = await users.findOne({ attributes: ["name"] });
@@ -15,6 +19,15 @@ app.get("/users2", async(req: express.Request, res: express.Response) => {
   console.log();
   const user = await Users2.findOne();
   res.send(user);
+});
+
+app.post("/users2", async(req: express.Request, res: express.Response) => {
+  const user = new Users2();
+  console.log("body", req.body);
+  user.name = req.body.name;
+  user.password = req.body.password;
+  user.save();
+  res.status(201).send();
 });
 
 export default app;
